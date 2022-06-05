9a451018fa80f61e562ae49795074a4efc605d13
Jose Luis Castro <76067475+jose9428@users.noreply.github.com>
2022-06-05 14:29:47 -0500

    Implementacion Fragment

---
+++ app/src/main/java/com/webfactor/jhonnatan/model/Pizza.java
@@ -0,0 +1,40 @@
+package com.webfactor.jhonnatan.model;
+
+public class Pizza {
+    private String imagen;
+    private String nombre;
+    private double precio;
+    private int stock;
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+}
--- app/src/main/java/com/webfactor/jhonnatan/view/fragments/RegistroPizzaFragment.java
+++ app/src/main/java/com/webfactor/jhonnatan/view/fragments/RegistroPizzaFragment.java
@@ -3,15 +3,20 @@ package com.webfactor.jhonnatan.view.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.webfactor.jhonnatan.R;
 
-public class RegistroPizzaFragment extends Fragment {
-
+public class RegistroPizzaFragment extends Fragment implements View.OnClickListener {
+    EditText txtPrecio, txtStock , txtNombre ;
+    Button btnGuardar;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +25,25 @@ public class RegistroPizzaFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_registro_pizza, container, false);
+        View view = inflater.inflate(R.layout.fragment_registro_pizza, container, false);
+        txtPrecio= (EditText) view.findViewById(R.id.lbRPrecio);
+        txtStock= (EditText) view.findViewById(R.id.lbRStock);
+        txtNombre= (EditText) view.findViewById(R.id.lbRNombrePizza);
+        btnGuardar = (Button) view.findViewById(R.id.btnGuardar);
+        btnGuardar.setOnClickListener(this);
+        return view;
+    }
+
+    public void GuardarPizza(){
+       Toast.makeText(getContext(),"OK" ,Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.btnGuardar:
+                GuardarPizza();
+                break;
+        }
     }
 }
